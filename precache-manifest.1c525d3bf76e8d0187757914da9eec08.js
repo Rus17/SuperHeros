aaa564e95f7ddb67a090e3d29efbf44442604666
Nazarenko Ruslan <rus.nazarenko@gmail.com>
2020-03-09 05:29:32 +0200

    Updates

---
+++ precache-manifest.1c525d3bf76e8d0187757914da9eec08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cd93874b2dcc6d1238b19961317538d",
+    "url": "/SuperHeros/index.html"
+  },
+  {
+    "revision": "23e9c176743b4e9e3bb7",
+    "url": "/SuperHeros/static/css/main.d2b6c2ea.chunk.css"
+  },
+  {
+    "revision": "bd777d8695fb347971b8",
+    "url": "/SuperHeros/static/js/2.03a4bd0f.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/SuperHeros/static/js/2.03a4bd0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23e9c176743b4e9e3bb7",
+    "url": "/SuperHeros/static/js/main.7e164cd4.chunk.js"
+  },
+  {
+    "revision": "8a311092ed74eff9bcc1",
+    "url": "/SuperHeros/static/js/runtime-main.497c934f.js"
+  }
+]);
